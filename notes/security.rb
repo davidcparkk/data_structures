4b35085420c923a1a133efeffcbf336a6590787a
David Park <xdavidpark@gmail.com>
2019-06-08 22:28:08 -0700

    Add MBA

---
+++ notes/security.rb
@@ -0,0 +1,13 @@
+********************
+MBA Security
+********************
+
+1. Which of the following can leave a website vulnerable to cross-site scripting (XSS) attacks?
+- embedding un-escaped user-created text inside a script tag
+- allowing users to create their own html which is stored and later rendered
+- loading un-escaped user-created text into an href value
+- loading un-escaped user-created text into a DOM element
+
+2. In order to keep application secrets (api keys, hash secrets, etc) safe, developers should:
+- never commit application secrets into the source code management
+- create specific environment variables for each application secret in the production environments
