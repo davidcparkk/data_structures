96775dd5760bf4e790e8f23c0d4b5295689a2eda
David Park <xdavidpark@gmail.com>
2019-06-14 23:46:48 -0700

    Stack and queue initial

--- recommended/2-1.js
+++ recommended/2-1.js
@@ -3,15 +3,17 @@
 // Write code to remove duplicates from an unsorted linked list.
 // How would you solve this problem if a temporary buffer is not allowed?
 
-const LinkedLIst = function(value) {
-  this.lvaue = value;
+// O(n) time
+
+const LinkedList = function(value) {
+  this.value = value;
   this.next = null;
 };
 
 const checkDups = function(head, node) {
   let currNode = head;
   while (currNode !== node) {
-    if (currNode.lvalue = node.value) {
+    if (currNode.value = node.value) {
       return true;
     }
     currNode = currNode.next;
