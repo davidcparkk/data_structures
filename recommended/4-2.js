a0ea6ec0bf51c23df0e67bb7779e1645ca9b7ef3
David Park <xdavidpark@gmail.com>
2019-06-24 23:59:32 -0700

    Add min BST

--- recommended/4-2.js
+++ recommended/4-2.js
@@ -1,3 +1,7 @@
 // Trees and Graphs
 // Minimal Tree
 // Given a sorted (increasing order) array with unique integer elements, write an algo to create a binary search tree with min height
+
+let insertBalanced = function(array) {
+  
+}
